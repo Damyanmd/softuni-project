e5e26564cc3b156a73b5f7eab458852939a9b9ea
Damyan Doykov <strikerbg98@gmail.com>
2021-10-21 00:32:28 +0300

    making changes for local server

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-841v2y8w_%wtwjp=&$4sf+%hhm0)vppc1%q*j-p(1p_=@4n*-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
+
 ALLOWED_HOSTS = [
     '*',
 ]
@@ -83,22 +84,14 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
+        'NAME': os.environ.get('DATABASE_NAME', 'petstagram'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
         'PORT': '5432',
     }
 }
 
-'''''
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'petstagram',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-'''''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
